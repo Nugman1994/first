8b12273bc4d4074780c1c77371c2aa52e33b5897
Nugman <nugmankuspakov@gmail.com>
2017-09-16 00:52:56 +0600

    v5

--- Program.cs
+++ Program.cs
@@ -15,7 +15,8 @@ namespace p3_4
             for (int b= 2; i < a + 1; i++)
             for (int с= 2; i < a + 1; i++)
             for (int d= 2; i < a + 1; i++)
-            эта прога без ошибок v4
+            for (int й= 2; i < a + 1; i++)
+            эта прога с ошибокой v5
     	}
     }      
 }
