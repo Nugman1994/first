0355ef631ce0d53144ff9a1889dda48e417ac771
Nugman <nugmankuspakov@gmail.com>
2017-09-16 00:53:42 +0600

    v6

--- Program.cs
+++ Program.cs
@@ -16,7 +16,8 @@ namespace p3_4
             for (int с= 2; i < a + 1; i++)
             for (int d= 2; i < a + 1; i++)
             for (int й= 2; i < a + 1; i++)
-            эта прога с ошибокой v5
+            for (int ц= 2; i < a + 1; i++)
+            эта прога с ошибокой v6
     	}
     }      
 }
